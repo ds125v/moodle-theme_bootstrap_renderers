a0b4e288aa2080407ee4e45441ee8138de31ddde
David Scotson <david.scotson@glasgow.ac.uk>
2012-10-04 10:13:50 +0100

    more raw bootswatches and a script to rotate them

---
+++ style/bootswapper.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+cp bootstrap.css default.css
+while true; do
+    sleep 60
+    cp amelia.css bootstrap.css
+    sleep 60
+    cp awesome.css bootstrap.css
+    sleep 60
+    cp cerulean.css bootstrap.css
+    sleep 60
+    cp cyborg.css bootstrap.css
+    sleep 60
+    cp journal.css bootstrap.css
+    sleep 60
+    cp readable.css bootstrap.css
+    sleep 60
+    cp simplex.css bootstrap.css
+    sleep 60
+    cp slate.css bootstrap.css
+    sleep 60
+    cp spacelab.css bootstrap.css
+    sleep 60
+    cp spruce.css bootstrap.css
+    sleep 60
+    cp superhero.css bootstrap.css
+    sleep 60
+    cp united.css bootstrap.css
+done
