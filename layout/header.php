42615d959d4c7fa468a0109a64a040bf69d2bf94
David Scotson <david.scotson@glasgow.ac.uk>
2012-10-05 14:22:22 +0100

    tidying up header to make theme settings easier

--- layout/header.php
+++ layout/header.php
@@ -1,4 +1,5 @@
 <?php
+
 $hasheading = ($PAGE->heading);
 $hasnavbar = (empty($PAGE->layout_options['nonavbar']) && $PAGE->has_navbar());
 $hasfooter = (empty($PAGE->layout_options['nofooter']));
@@ -23,59 +24,70 @@ if ($showsidepre && !$showsidepost) {
 if ($hascustommenu) {
     $bodyclasses[] = 'has_custom_menu';
 }
+$bodyclasses = s($PAGE->bodyclasses.' '.join(' ', $bodyclasses));
+
+$html5shiv = new moodle_url('/theme/bootstrap_renderers/html5shiv.js');
+$html5shiv = "<script src='$html5shiv'></script>";
+
+$favicon_url = $OUTPUT->pix_url('favicon', 'theme');
+
+$random = true;
+if ($random) {
+    $navbar_inverse = rand(0, 1);
+    $fluid = rand(0, 1);
+}
+
+$navbar_inverse = $navbar_inverse ? 'navbar-inverse' : '';
+$fluid = $fluid ? '-fluid' : '';
+
+$header = '';
+if ($hasheading || $hasnavbar) {
+    $header .= "<header>";
+    if ($hasheading) {
+        if (!empty($PAGE->layout_options['langmenu'])) {
+            $header .= $OUTPUT->lang_menu();
+        }
+        $header .= $PAGE->headingmenu;
+    }
+    if ($hasnavbar) {
+        $navbar = $OUTPUT->navbar();
+        $button = $PAGE->button;
+        $navbar = "<div>$navbar<div class=pull-right>$button</div></div>";
+        $header .= $navbar;
+    }
+    $header .= "</header>";
+}
+/* end of settings */
+
 echo $OUTPUT->doctype() ?>
 <html <?php echo $OUTPUT->htmlattributes() ?>>
 <head>
 	<meta charset="utf-8">
  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title><?php echo $PAGE->heading ?></title>
-    
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <link rel="shortcut icon" href="<?php echo $OUTPUT->pix_url('favicon', 'theme')?>">
-    
-    <!--[if lt IE 9]>
-    <script src="<?php echo new moodle_url('/theme/bootstrap_renderers/html5shiv.js') ?>"></script>
-	<![endif]-->
-
+    <title><?php echo $PAGE->heading ?></title>
+    <link rel="shortcut icon" href="<?php echo $favicon_url ?>">
+    <!--[if lt IE 9]><?php echo $html5shiv ?><![endif]-->
     <?php echo $OUTPUT->standard_head_html(); ?>
 </head>
 
+<body id="<?php echo $PAGE->bodyid ?>" class="<?php $bodyclasses?>">
 
-<body id="<?php echo $PAGE->bodyid ?>" class="<?php echo $PAGE->bodyclasses?>">
-<div class="container-fluid">
-    <div class="navbar navbar-inverse navbar-top">
-        <div class="navbar-inner">
-            <a class="brand" href="<?php echo new moodle_url("/")?>">Moodle (short?)name here</a>
-            <ul class="nav">
-              <li class="active"><a href="<?php echo new moodle_url("/")?>">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#contact">Contact</a></li>
-            </ul>
-            <form class="navbar-form pull-right navbar-text">
+<div class="container<?php echo $fluid ?>">
+
+<div class="navbar <?php echo $navbar_inverse ?> navbar-top">
+    <div class="navbar-inner">
+        <a class="brand" href="<?php echo new moodle_url("/")?>"><?php echo $PAGE->heading?></a>
+        <ul class="nav">
+            <li class="active"><a href="<?php echo new moodle_url("/")?>">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+        <form class="navbar-form pull-right navbar-text">
               <?php echo $OUTPUT->login_info() ?> 
-            </form> 
-        </div>
+        </form> 
     </div>
+</div>
+<?php
 
-
-<?php if ($hasheading || $hasnavbar) : ?>
-    <header>    	
-        <?php if ($hasheading) { ?>
-        <h1><a href="<?php  global $CFG; $url = $CFG->wwwroot."/course/view.php?id=".$PAGE->course->id; echo $url; ?>"><?php echo $PAGE->heading ?></a></h1>
-        
-        <?php if (!empty($PAGE->layout_options['langmenu'])) {
-                echo $OUTPUT->lang_menu();
-            }
-            echo $PAGE->headingmenu
-        ?><?php } ?>
-        
-        
-        <?php if ($hasnavbar) { ?>
-            <div>
-                <?php echo $OUTPUT->navbar(); ?>
-                <div class="pull-right"> <?php echo $PAGE->button; ?></div>
-            </div>
-        <?php } ?>
-    </header>
-<?php endif; ?>
+echo $header;
