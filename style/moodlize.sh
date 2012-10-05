0918a262319a040fa14a86def6f4f133f996a4c6
David Scotson <david.scotson@glasgow.ac.uk>
2012-10-05 13:56:32 +0100

    including upstream bootswatches unchanged

--- style/moodlize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# rewrite glyphicon location
-sed -i -e's_../img/glyphicons-halflings.png_[[pix:theme|glyphicons-halflings]]_' \
-       -e's_../img/glyphicons-halflings-white.png_[[pix:theme|glyphicons-halflings-white]]_' *.css
-
-# rewrite font location for font-awesome
-sed -i 's_/font/_/theme/bootstrap\_renderers/font/_g' awesome.css
