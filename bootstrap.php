a6770f6f013d3ba5f7acc906560ebd26a83ef989
David Scotson <david.scotson@glasgow.ac.uk>
2012-10-08 16:45:12 +0100

    try differnt search button style

--- bootstrap.php
+++ bootstrap.php
@@ -147,11 +147,10 @@ class bootstrap {
         return html::form($form_attributes, html::input($input_attributes) . html::submit(array('value'=>$submit_text)));
     }
     public static function inline_search_append($action, $placeholder, $value, $submit_text) {
-        $form_attributes['class'] = 'form-search';
+        $form_attributes['class'] = 'inline-form';
         $form_attributes['method'] = 'get';
         $form_attributes['action'] = $action;
 
-        $input_attributes['class'] = 'search-query';
         $input_attributes['type'] = 'text';
         $input_attributes['name'] = 'query';
         $input_attributes['placeholder'] = $placeholder;
