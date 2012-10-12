95a82d91c249b5d80b1fe52b8b446361cc633fbc
David Scotson <david.scotson@glasgow.ac.uk>
2012-10-12 15:14:04 +0100

    changed hardcoded path to $theme->dir, closes #5

--- lib.php
+++ lib.php
@@ -27,7 +27,7 @@ function processor($css, $theme) {
         $awesome = rand(0, 1);
     }
 
-    $themedir = "/var/www/bootstrap/theme/bootstrap_renderers";
+    $themedir = $theme->dir;
 
     $find[] = "[[bootstrap]]";
     $replace[] = file_get_contents("$themedir/style/$subtheme/bootstrap.css");
