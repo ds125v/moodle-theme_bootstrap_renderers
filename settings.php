95ec1a0a82b412b78a53f71c687b273635a5c250
David Scotson <david.scotson@glasgow.ac.uk>
2012-10-23 16:17:38 +0100

    whitespace tabs->4 spaces

--- settings.php
+++ settings.php
@@ -3,10 +3,10 @@
 defined('MOODLE_INTERNAL') || die;
 
 if ($ADMIN->fulltree) {
-	
-	$themes = array(
-		'random' => 'Random',
-	    'amelia' => 'Amelia',
+
+    $themes = array(
+        'random' => 'Random',
+        'amelia' => 'Amelia',
         'bootstrap' => 'Bootstrap',
         'cerulean' => 'Cerulean',
         'cyborg' => 'Cyborg',
@@ -18,52 +18,52 @@ if ($ADMIN->fulltree) {
         'spruce' => 'Spruce',
         'superhero' => 'Superhero',
         'united' => 'United',
-	);
-	
-    // Sub Theme Dropdown
+    );
+
+    // Sub Theme Dropdown.
     $name = 'theme_bootstrap_renderers/subtheme';
-    $title = get_string('subtheme','theme_bootstrap_renderers');
+    $title = get_string('subtheme', 'theme_bootstrap_renderers');
     $description = get_string('subthemedesc', 'theme_bootstrap_renderers');
     $default = 'random';
-	$choices = $themes;
+    $choices = $themes;
     $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
     $settings->add($setting);
-	
-	// Fluid Layout
+
+    // Fluid Layout.
     $name = 'theme_bootstrap_renderers/fluid';
-    $title = get_string('fluid','theme_bootstrap_renderers');
+    $title = get_string('fluid', 'theme_bootstrap_renderers');
     $description = get_string('fluiddesc', 'theme_bootstrap_renderers');
     $default = '1';
     $setting = new admin_setting_configcheckbox($name, $title, $description, $default);
     $settings->add($setting);
-	
-	// Responsive Layout
+
+    // Responsive Layout.
     $name = 'theme_bootstrap_renderers/responsive';
-    $title = get_string('responsive','theme_bootstrap_renderers');
+    $title = get_string('responsive', 'theme_bootstrap_renderers');
     $description = get_string('responsivedesc', 'theme_bootstrap_renderers');
     $default = '1';
     $setting = new admin_setting_configcheckbox($name, $title, $description, $default);
     $settings->add($setting);
-	
-	// Fixed Navbar
+
+    // Fixed Navbar.
     $name = 'theme_bootstrap_renderers/fixed';
-    $title = get_string('fixed','theme_bootstrap_renderers');
+    $title = get_string('fixed', 'theme_bootstrap_renderers');
     $description = get_string('fixeddesc', 'theme_bootstrap_renderers');
     $default = '0';
     $setting = new admin_setting_configcheckbox($name, $title, $description, $default);
     $settings->add($setting);
-	
-	// Invert Navigation Bar
+
+    // Invert Navigation Bar.
     $name = 'theme_bootstrap_renderers/navbarinvert';
-    $title = get_string('navbarinvert','theme_bootstrap_renderers');
+    $title = get_string('navbarinvert', 'theme_bootstrap_renderers');
     $description = get_string('navbarinvertdesc', 'theme_bootstrap_renderers');
     $default = '0';
     $setting = new admin_setting_configcheckbox($name, $title, $description, $default);
     $settings->add($setting);
-	
-	// Enable Awesome Font
+
+    // Enable Awesome Font.
     $name = 'theme_bootstrap_renderers/awesome';
-    $title = get_string('awesomefont','theme_bootstrap_renderers');
+    $title = get_string('awesomefont', 'theme_bootstrap_renderers');
     $description = get_string('awesomefontdesc', 'theme_bootstrap_renderers');
     $default = '1';
     $setting = new admin_setting_configcheckbox($name, $title, $description, $default);
