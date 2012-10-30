d4210069285318d88844396c36373eb2d6e1a892
David Scotson <david.scotson@glasgow.ac.uk>
2012-10-30 13:19:21 +0000

    the actual theme name

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2012103000;
-$plugin->component = 'theme_zebra';
+$plugin->component = 'theme_bootstrap_renderers';
 $plugin->requires  = 2010112400;
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '0.0.1 (Build: 2012103000)';
