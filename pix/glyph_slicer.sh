a0f11d0df5ca6cef7576efdaf300feb1747c6a0f
David Scotson <david.scotson@glasgow.ac.uk>
2012-11-02 17:03:01 +0000

    retain transparent background on icons

--- pix/glyph_slicer.sh
+++ pix/glyph_slicer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-convert -page 480x168+5+5 $1 -flatten with_padding.png
+convert -page 480x168+5+5 -background transparent $1 -flatten with_padding.png
 
 convert with_padding.png -crop 20x7@ +repage +adjoin glyphs+1_%d.png
 
@@ -13,15 +13,20 @@ cp pix/t/hide.png pix/i/hide.png
 mv glyphs+2_6.png pix/t/show.png
 cp pix/t/show.png pix/i/show.png
 mv glyphs+2_15.png pix/t/expanded.png
-mv glyphs+2_20.png pix/i/move.png
+mv glyphs+2_20.png pix/t/move.png
 mv glyphs+2_39.png pix/i/settings.png
 mv glyphs+2_41.png pix/i/filter.png
 mv glyphs+2_44.png pix/t/delete.png
-mv glyphs+2_57.png pix/i/backup.png
-mv glyphs+2_58.png pix/i/restore.png
+mv glyphs+2_57.png pix/t/backup.png
+mv glyphs+2_58.png pix/t/restore.png
+mv glyphs+2_85.png pix/i/info.png
+mv glyphs+2_95.png pix/i/marked.png
 mv glyphs+2_96.png pix/t/editstring.png
 mv glyphs+2_100.png pix/i/edit.png
-mv glyphs+2_103.png pix/t/move_2d.png
+cp pix/i/edit.png pix/t/edit.png
+mv glyphs+2_101.png pix/i/cohort.png
+mv glyphs+2_103.png pix/i/move_2d.png
+mv glyphs+2_106.png pix/i/marker.png
 mv glyphs+2_116.png pix/t/collapsed.png
 mv glyphs+2_117.png pix/i/users.png
 cp pix/i/users.png pix/i/roles.png
@@ -33,5 +38,7 @@ mv glyphs+2_130.png pix/t/right.png
 mv glyphs+2_131.png pix/t/up.png
 mv glyphs+2_132.png pix/t/down.png
 mv glyphs+2_136.png pix/t/add.png
+cp pix/t/add.png pix/t/enroladd.png
 mv glyphs+2_137.png pix/t/minus.png
+
 # some irregular ones
