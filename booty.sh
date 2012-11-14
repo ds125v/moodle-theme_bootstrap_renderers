f8817fd5a95fa5de7606151a7a3d2dce8c18dac5
David Scotson <david.scotson@glasgow.ac.uk>
2012-11-14 12:40:09 +0000

    first draft of automated theme creator

---
+++ booty.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir ../$1
+cp -r ../standard/* ../$1/.
+
+cp html5shiv.js ../$1/.
+cp renderers.php ../$1/.
+mkdir ../$1/renderers
+cp renderers/*.php ../$1/renderers/.
+mkdir ../$1/layout
+cp layout/*.php ../$1/layout/.
+mkdir ../$1/javascript
+cp -r javascript/* ../$1/javascript/.
+
+sed -i s/theme_bootstrap_renderers/theme_$1/ ../renderers/*.php
+
+cp booty/config.php ../$1/.
+sed -i s/booty/$1/ ../$1/config.php
+
+rm ../$1/lang/en/theme_standard.php
+cp booty/theme_booty.php ../$1/lang/en/theme_$1.php.
+
+cp booty/booty.css ../$1/style/$1.css
---
+++ booty/config.php
@@ -0,0 +1,91 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Configuration for Moodle's standard theme.
+ *
+ * This theme is the default theme within Moodle 2.0, it builds upon the base theme
+ * adding only CSS to create the simple look and feel Moodlers have come to recognise.
+ *
+ * For full information about creating Moodle themes, see:
+ *  http://docs.moodle.org/dev/Themes_2.0
+ *
+ * @package   moodlecore
+ * @copyright 2010 Sam Hemelryk
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$THEME->name = 'booty';
+$THEME->parents = array('base');
+$THEME->sheets = array(
+    'core',     /** Must come first**/
+    'admin',
+    'blocks',
+    'calendar',
+    'course',
+    'user',
+    'dock',
+    'grade',
+    'message',
+    'modules',
+    'question',
+    'css3',      /** Sets up CSS 3 + browser specific styles **/
+    'booty',
+);
+$THEME->enable_dock = true;
+$THEME->rendererfactory = 'theme_overridden_renderer_factory';
+$THEME->javascripts_footer = array('jquery', 'bootstrap');
+$THEME->doctype = 'html5';
+$basic = array(
+    'file' => 'general.php',
+);
+$normal = $basic + array(
+    'regions' => array('side-pre', 'side-post'),
+    'defaultregion' => 'side-post',
+);
+$one_column = $basic + array(
+    'regions' => array('side-pre'),
+    'defaultregion' => 'side-pre',
+);
+$plain = $basic + array(
+    'options' => array(
+        'noblocks'=>true,
+        'nofooter'=>true,
+        'nonavbar'=>false,
+        'nocustommenu'=>true,
+    ),
+);
+
+$THEME->layouts = array(
+    'admin' => $one_column,
+    'base' => $basic,
+    'course' => $normal,
+    'coursecategory' => $normal,
+    'embedded' => $basic + array('options' => array('nofooter'=>true, 'nonavbar'=>true, 'nocustommenu'=>true)),
+    'frametop' => $basic + array('options' => array('nofooter'=>true)),
+    'frontpage' => $normal,
+    'incourse' => $normal,
+    'login' => $basic,
+    'maintenance' => $plain,
+    'mydashboard' => $normal,
+    'mypublic' => $normal,
+    'popup' => $basic + array('options' => array('nofooter'=>true, 'nonavbar'=>true, 'nocustommenu'=>true, 'nologininfo'=>true)),
+    'print' => $plain,
+    'redirect' => $plain,
+    'report' => $one_column,
+    'standard' => $normal,
+);
