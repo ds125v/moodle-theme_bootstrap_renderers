b8abbe4d94d3195a3fc5fd76b827c496c5003b43
David Scotson <david.scotson@glasgow.ac.uk>
2013-01-17 21:32:59 +0000

    up the version

--- version.php
+++ version.php
@@ -2,8 +2,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2012103000;
+$plugin->version   = 20130107000;
 $plugin->component = 'theme_bootstrap_renderers';
 $plugin->requires  = 2010112400;
 $plugin->maturity  = MATURITY_ALPHA;
-$plugin->release   = '0.0.1 (Build: 2012103000)';
+$plugin->release   = '0.0.3 (Build: 20130107000)';
