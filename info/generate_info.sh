48578d1aa80b96584bd053845d5736016c3384a6
David Scotson <david.scotson@glasgow.ac.uk>
2013-01-22 09:52:05 +0000

    update renderer list for 2.4

--- info/generate_info.sh
+++ info/generate_info.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# output of, in moodle root:
+# output of, in moodle root of 2.4/5:
 git grep --full-name -p --break --heading -E '(public|private|protected) function' -- '*/renderer.php' 'lib/outputrenderers.php' > list_renderers.txt
-# output of, in root of theme directory, on dev branch:
+# output of, in root of theme's info directory, on master branch:
 git grep --full-name -p --break --heading -E '(public|private|protected) function' -- '../*renderer.php' > progress.txt
 
 
